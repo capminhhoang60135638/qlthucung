6e763f12da514a5ee34a7a8dfe8d9a1cf45cd20c
Minh Hoang <minhhoangcap@gmail.com>
2021-10-29 15:48:21 +0700

    Chinh sua trang them khach hàng

--- khachhang/themkhachhang.php
+++ khachhang/themkhachhang.php
@@ -163,40 +163,14 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 		$sdt=trim($_POST['sdt']);
 	}
 
-	// //cap nhat thanh phan dinh duong va loi ich
-	// $tp_dinh_duong=$_POST['tp_dinh_duong'];
-	// $loi_ich=$_POST['loi_ich'];
-	// //kiểm tra hình sản phẩm và thực hiện upload file
-	// if($_FILES['hinh']['name']!=""){
-	// 	$hinh=$_FILES['hinh'];
-	// 	$ten_hinh=$hinh	['name'];
-	// 	$type=$hinh['type'];
-	// 	$size=$hinh['size'];
-	// 	$tmp=$hinh['tmp_name'];
-	// 	if(($type=='image/jpeg' || ($type=='image/bmp') || ($type=='image/gif') && $size<8000))
-	// 	{
-	// 		move_uploaded_file($tmp,"Hinh_sua/".$ten_hinh);
-	// 	}
-	// }
+	
 	if(empty($errors))//neu khong co loi xay ra
 	{ 
 		$query="INSERT INTO khach_hang VALUES ('$makh','$tenkh','$namsinh','$diachi','$sdt')";
 		$result=mysqli_query($dbc,$query);
 		if(mysqli_affected_rows($dbc)==1){//neu them thanh cong
 			echo "<div align='center'>Thêm mới thành công!</div>";			
-			// $query="Select sua.*, Ten_hang_sua from Sua,hang_sua WHERE sua.ma_hang_sua=hang_sua.ma_hang_sua
-			// 		AND ma_sua ='". $ma_sua. "'";
-			// $result=mysqli_query($dbc,$query);
-			// if(mysqli_num_rows($result)==1)
-			// {	$row=mysqli_fetch_array($result, MYSQLI_ASSOC);
-				// echo '<table align="center" border="1" cellpadding="5" cellspacing="5" style="border-collapse:collapse;">
-				// 		<tr bgcolor="#eeeeee"><td colspan="2" align="center"><h3>'.$row['Ten_sua'].' - '.$row['Ten_hang_sua'].'</h3></td></tr>';
-				// 	echo '<tr><td><img src="Hinh_sua/'.$row['Hinh'].'"/></td>';
-				// 				echo '<td><i><b>Thành phần dinh dưỡng:</i></b><br />'.$row['TP_Dinh_Duong'].'<br />';
-				// 				echo '<i><b>Lợi ích:</b></i>'.$row['Loi_ich'].'<br />';
-				// 				echo '<i><b>Trọng lượng: </b></i>'.$row['Trong_luong'].' gr - <i><b>Đơn giá: </b></i>'.$row['Don_gia'].' VNĐ';
-				// 						echo '</td></tr></table>';				
-			//}
+			
 		}
 		else //neu khong them duoc
 		{
